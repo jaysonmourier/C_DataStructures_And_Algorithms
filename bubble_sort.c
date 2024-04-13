90935b4f35c09904cb89bdd5ff521abe7a9f8c88
Jayson Mourier <mourierjayson@protonmail.com>
2024-04-13 10:46:52 +0200

    added 'const'

--- bubble_sort.c
+++ bubble_sort.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-void sort(int *table, int size)
+void sort(int *table, const int size)
 {
     int sorted = 1;
     for (int i = size - 1; i > 0; --i)
@@ -20,7 +20,7 @@ void sort(int *table, int size)
     }
 }
 
-void print_table(int *table, int size)
+void print_table(int *table, const int size)
 {
     for (int i = 0; i < size; ++i)
     {
